314386236a5809ad10bbee17190b7b4782553549
Ben-Arlanda <ben.arlanda@protonmail.com>
2024-04-06 13:54:03 +1100

    added hamburger menu popup for small screens

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 import menuIcon from '../assets/menu-icon.svg'
+import closeIcon from '../assets/close-icon.svg'
 
 // Link component for navigation
 const Link = ({ page, selectedPage, setSelectedPage }) => {
@@ -33,7 +34,7 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
     <nav className={`z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className={`font-sans text-3xl font-bold`}>BA</h4>
-        
+
         {/* Render navigation links if screen size is above 768px */}
         {isAboveSmallScreens ? (
           <div className="flex justify-between gap-16 font-sans text-sm font-semibold">
@@ -65,10 +66,51 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
           </div>
         ) : (
           <button
-          onClick={() => setIsMenuToggled (!isMenuToggled)}
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
           >
-              <img src={menuIcon} alt="menu-icon" />
+            <img src={menuIcon} alt="menu-icon" />
           </button>
+        )}
+
+        {/* HAMBURGER POPUP MENU */}
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-deep-blue">
+
+            {/* CLOSE ICON */}
+            <div className="absolute top-0 right-0 p-4">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img src={closeIcon} alt="close-icon" />
+              </button>
+            </div>
+
+            <div className="flex flex-col items-center gap-10">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="About"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Experience"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
         )}
       </div>
     </nav>
