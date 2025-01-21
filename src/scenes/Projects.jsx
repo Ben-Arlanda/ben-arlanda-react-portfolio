13c120ad010d3eb7c59a0efa6adc368991a29680
Ben-Arlanda <ben.arlanda@protonmail.com>
2025-01-21 15:14:31 +1100

    fixing mobile view of projects

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -76,8 +76,8 @@ const Projects = () => {
 					transition={{ duration: 1 }}
 					custom={
 						window.innerWidth < 768
-							? { hiddenX: -150, visibleX: 0 }
-							: { hiddenX: -350, visibleX: 50 }
+							? { hiddenX: -50, visibleX: 0 }
+							: { hiddenX: -200, visibleX: 50 }
 					}
 					variants={{
 						hidden: (custom) => ({ opacity: 0, x: custom.hiddenX }),
